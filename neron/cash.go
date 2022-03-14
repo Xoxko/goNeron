03b481f89d5c38cf70b81f6f83994b7ee1b48858
Xoxko <hohko@outlook.com>
2022-03-14 22:17:52 +0300

    Завирешил обратное рапостроение ошибки

--- neron/cash.go
+++ neron/cash.go
@@ -1,5 +1,10 @@
 package neron
 
+import (
+	"fmt"
+	"math"
+)
+
 // init struct dataset
 var Neron neron_cash
 
@@ -7,8 +12,9 @@ type neron_cash struct {
 	NODE [][]node
 }
 type node struct {
-	W []float64
-	H float64
+	W     []float64
+	H     float64
+	delta float64
 }
 
 /*Создание массива NODE
@@ -57,7 +63,11 @@ func (nerv *neron_cash) Random(number func() float64) {
 	}
 }
 
-func (nerv *neron_cash) Calculation(sigmoid func(x float64) float64) {
+/*Зависит -> NewInitNode -> Random
+Производит расчет неросети
+*/
+func (nerv *neron_cash) Calculation() {
+
 	coppy := nerv.NODE[1:]
 	for x := range coppy {
 		for y := range coppy[x] {
@@ -65,11 +75,61 @@ func (nerv *neron_cash) Calculation(sigmoid func(x float64) float64) {
 			for w := range coppy[x][y].W {
 				coppy[x][y].H += coppy[x][y].W[w] * nerv.NODE[x][w].H
 			}
-			coppy[x][y].sigmoid(sigmoid)
+			coppy[x][y].H = sigma(coppy[x][y].H)
 		}
 	}
 }
 
-func (node *node) sigmoid(sigmoid func(x float64) float64) {
-	node.H = sigmoid(node.H)
+//сигмоида
+func sigma(x float64) float64 {
+	return math.Tanh(x) //1.0 / (1.0 + math.Exp(-x))
+}
+
+//производная от сигмоиды
+func dsigma(x float64) float64 {
+	return 1.0 - math.Pow(x, 2) //x * (1.0 - x)
+}
+
+/*обучение неросети
+реализован по градиентному спуску
+*/
+func (nerv *neron_cash) Training(ExitError []float64, n float64) {
+	if len(ExitError) != len(nerv.NODE[len(nerv.NODE)-1]) {
+		panic(fmt.Errorf("ExitError"))
+	}
+	coppy := nerv.NODE[1:]
+	i := len(coppy) - 1
+
+	//расчет ошибки d выходного слоя сети.
+	for y := range ExitError {
+		coppy[i][y].delta = ExitError[y] - coppy[i][y].H
+	}
+	//распространение сигнала ошибки d по всем узлам
+	for x := range coppy {
+		for y := range coppy[i-x] {
+			for w := range coppy[i-x][y].W {
+				nerv.NODE[i-x][w].delta = coppy[i-x][y].W[w] * coppy[i-x][y].delta
+			}
+		}
+	}
+	//корректировка весовых коэффициентов
+	for x := range coppy {
+		for y := range coppy[x] {
+			for w := range coppy[x][y].W {
+				coppy[x][y].W[w] += nerv.NODE[x][w].H * coppy[x][y].delta * n * dsigma(coppy[x][y].H)
+			}
+		}
+	}
+
+}
+
+//очищает неросеть от вычисления
+func (nerv *neron_cash) Clear() {
+	coppy := nerv.NODE
+	for x := range coppy {
+		for y := range coppy[x] {
+			coppy[x][y].H = 0
+			coppy[x][y].delta = 0
+		}
+	}
 }
